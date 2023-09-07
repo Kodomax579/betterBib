7c4e868afa7cd38512b5c2ccba4dff083d56b2f6
Kodomax579 <engelhardtcarsten00@gmail.com>
2023-09-07 20:36:25 +0200

    Books can now get borrowed

--- src/Menue/singleBook_GUI.java
+++ src/Menue/singleBook_GUI.java
@@ -1,5 +1,7 @@
 package Menue;
 
+import static javax.swing.JOptionPane.showMessageDialog;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -7,6 +9,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import main.MySQL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -20,6 +24,8 @@ import javax.swing.JEditorPane;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
+import java.text.DateFormat;
 
 public class singleBook_GUI extends JFrame {
 
@@ -42,6 +48,16 @@ public class singleBook_GUI extends JFrame {
 		String isbn= array.get(2);
 		String genre= array.get(3);
 		String discription= array.get(4);
+		String customer_ID = array.get(5);
+		
+		if(customer_ID != null)
+		{
+			customer_ID = "Not Available";
+		}
+		else
+		{
+			customer_ID = "Available";
+		}
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 500);
@@ -74,35 +90,35 @@ public class singleBook_GUI extends JFrame {
 		
 		JLabel label = new JLabel("Genre");
 		label.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		label.setBounds(43, 149, 110, 34);
+		label.setBounds(43, 116, 110, 34);
 		contentPane.add(label);
 		
 		JLabel label2 = new JLabel("ISBN:");
 		label2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		label2.setBounds(43, 224, 110, 34);
+		label2.setBounds(43, 173, 110, 34);
 		contentPane.add(label2);
 		
 		JLabel label3 = new JLabel("Discription:");
 		label3.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		label3.setBounds(43, 310, 110, 34);
+		label3.setBounds(43, 246, 110, 34);
 		contentPane.add(label3);
 		
 		JLabel Genre = new JLabel(genre);
 		Genre.setHorizontalAlignment(SwingConstants.CENTER);
 		Genre.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		Genre.setBounds(164, 149, 513, 44);
+		Genre.setBounds(164, 111, 513, 44);
 		contentPane.add(Genre);
 		
 		JLabel ISBN = new JLabel(isbn);
 		ISBN.setHorizontalAlignment(SwingConstants.CENTER);
 		ISBN.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		ISBN.setBounds(164, 224, 513, 44);
+		ISBN.setBounds(164, 168, 513, 44);
 		contentPane.add(ISBN);
 		
 		JTextPane textPane = new JTextPane();
 		textPane.setEditable(false);
 		textPane.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		textPane.setBounds(174, 279, 513, 118);
+		textPane.setBounds(174, 223, 513, 118);
 		contentPane.add(textPane);
 		textPane.setText(discription);
 		
@@ -120,17 +136,41 @@ public class singleBook_GUI extends JFrame {
 		btnNewButton.setBounds(127, 416, 130, 34);
 		contentPane.add(btnNewButton);
 		
+		JLabel lblAccesib = new JLabel("Available:");
+		lblAccesib.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblAccesib.setBounds(43, 354, 110, 34);
+		contentPane.add(lblAccesib);
+		
+		JLabel available = new JLabel(customer_ID);
+		available.setHorizontalAlignment(SwingConstants.CENTER);
+		available.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		available.setBounds(184, 349, 513, 44);
+		contentPane.add(available);
+		
 		JButton btnNewButton_1 = new JButton("lend");
 		btnNewButton_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) 
 			{
-				
+				if( available.getText() == "Available")
+				{
+					boolean lend =MySQL.lend(ID,login);
+					if(lend)
+					{
+						MySQL.historyBorrow(ID, login);
+						showMessageDialog(null, "You Borrowed the book");
+						Menue_GUI menue = new Menue_GUI(login);
+						menue.setVisible(true);
+						dispose();
+					}
+				}
+				else {
+					showMessageDialog(null, "Not Available");
+				}	
 			}
 		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnNewButton_1.setBounds(440, 416, 130, 34);
 		contentPane.add(btnNewButton_1);
-		
 	}
 }
